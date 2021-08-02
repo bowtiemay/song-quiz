734a92beae1dd893f418aa0b0df211d6415086e6
Maya Cobb <62029215+mayacobb@users.noreply.github.com>
2021-08-02 10:59:32 -0700

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,7 @@
+<link rel = "stylesheet" href = "prj1.css">
+
+<?php
+  function printHeading() {
+    echo "<div class=\"fixed-bg\"><h1>What Phoebe Bridgers Song Are You?</h1><br><br><img src=\"logo.png\" alt=\"atdp\" width = 100></div>";
+  }
+ ?>
